b62355568f373ed412e66bbb3cfec5d732c02d50
Marko <marko.stojiljkovic.ekis@gmail.com>
2018-10-15 11:39:43 +0200

    Added default values in debug tab, calibrated some devices

--- SupercapController/Calibrate.Designer.cs
+++ SupercapController/Calibrate.Designer.cs
@@ -39,30 +39,27 @@
             // 
             // buttonTakeSample
             // 
-            this.buttonTakeSample.Location = new System.Drawing.Point(16, 69);
-            this.buttonTakeSample.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonTakeSample.Location = new System.Drawing.Point(12, 56);
             this.buttonTakeSample.Name = "buttonTakeSample";
-            this.buttonTakeSample.Size = new System.Drawing.Size(108, 28);
-            this.buttonTakeSample.TabIndex = 0;
+            this.buttonTakeSample.Size = new System.Drawing.Size(81, 23);
+            this.buttonTakeSample.TabIndex = 1;
             this.buttonTakeSample.Text = "Take Sample";
             this.buttonTakeSample.UseVisualStyleBackColor = true;
             this.buttonTakeSample.Click += new System.EventHandler(this.buttonTakeSample_Click);
             // 
             // textBoxFixedVoltage
             // 
-            this.textBoxFixedVoltage.Location = new System.Drawing.Point(16, 38);
-            this.textBoxFixedVoltage.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxFixedVoltage.Location = new System.Drawing.Point(12, 31);
             this.textBoxFixedVoltage.Name = "textBoxFixedVoltage";
-            this.textBoxFixedVoltage.Size = new System.Drawing.Size(107, 22);
-            this.textBoxFixedVoltage.TabIndex = 1;
+            this.textBoxFixedVoltage.Size = new System.Drawing.Size(81, 20);
+            this.textBoxFixedVoltage.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 18);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(9, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(130, 17);
+            this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Fixed voltage in mV";
             // 
@@ -71,20 +68,20 @@
             this.textBoxDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxDisplay.Location = new System.Drawing.Point(165, 18);
-            this.textBoxDisplay.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxDisplay.Location = new System.Drawing.Point(124, 15);
             this.textBoxDisplay.Multiline = true;
             this.textBoxDisplay.Name = "textBoxDisplay";
-            this.textBoxDisplay.Size = new System.Drawing.Size(496, 271);
+            this.textBoxDisplay.Size = new System.Drawing.Size(368, 415);
             this.textBoxDisplay.TabIndex = 3;
+            this.textBoxDisplay.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(16, 106);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(12, 86);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(108, 28);
+            this.button1.Size = new System.Drawing.Size(81, 23);
             this.button1.TabIndex = 5;
+            this.button1.TabStop = false;
             this.button1.Text = "Clear";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -93,28 +90,32 @@
             // 
             this.textBoxGain.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.textBoxGain.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxGain.Location = new System.Drawing.Point(174, 297);
+            this.textBoxGain.Location = new System.Drawing.Point(130, 435);
+            this.textBoxGain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxGain.Name = "textBoxGain";
             this.textBoxGain.ReadOnly = true;
-            this.textBoxGain.Size = new System.Drawing.Size(144, 15);
+            this.textBoxGain.Size = new System.Drawing.Size(108, 13);
             this.textBoxGain.TabIndex = 6;
+            this.textBoxGain.TabStop = false;
             this.textBoxGain.Text = "Gain";
             // 
             // buttonCopyGain
             // 
-            this.buttonCopyGain.Location = new System.Drawing.Point(16, 142);
+            this.buttonCopyGain.Location = new System.Drawing.Point(12, 115);
+            this.buttonCopyGain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonCopyGain.Name = "buttonCopyGain";
-            this.buttonCopyGain.Size = new System.Drawing.Size(107, 28);
+            this.buttonCopyGain.Size = new System.Drawing.Size(80, 23);
             this.buttonCopyGain.TabIndex = 7;
+            this.buttonCopyGain.TabStop = false;
             this.buttonCopyGain.Text = "Copy Gain";
             this.buttonCopyGain.UseVisualStyleBackColor = true;
             this.buttonCopyGain.Click += new System.EventHandler(this.buttonCopyGain_Click);
             // 
             // Calibrate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(679, 321);
+            this.ClientSize = new System.Drawing.Size(504, 455);
             this.Controls.Add(this.buttonCopyGain);
             this.Controls.Add(this.textBoxGain);
             this.Controls.Add(this.button1);
@@ -122,7 +123,6 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxFixedVoltage);
             this.Controls.Add(this.buttonTakeSample);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Calibrate";
             this.Text = "Calibrate";
             this.ResumeLayout(false);
